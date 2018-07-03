15727c95b757bdbe3ed50416315bedf95ad70263
Noor Mohamed <noor@valiantsystems.com>
2018-07-03 19:15:13 +0530

    one

--- pms/pms/doctype/bug_sheet/bug_sheet_list.js
+++ pms/pms/doctype/bug_sheet/bug_sheet_list.js
@@ -55,7 +55,7 @@ function gi(listview) {
 function vi(listview) {
         var zTreeObj;
         function myOnClick(event, treeId, treeNode) {
-            console.log(listview)
+            
            var day = '';
                     $('[data-fieldname="project"]').val(null);
                     $('[data-fieldname="module"]').val(null);
@@ -66,17 +66,20 @@ function vi(listview) {
                     $('[data-fieldname="project"]').val(treeNode.idname);
                     $('[data-fieldname="module"]').val(null);
                     $('[data-fieldname="screen"]').val(null);
+                    listview.refresh();
                     cur_list.refresh();
                     break;
                 case 'module':
                     $('[data-fieldname="project"]').val(treeNode.getParentNode().idname);
                     $('[data-fieldname="module"]').val(treeNode.idname);
+                    listview.refresh();
                     cur_list.refresh();
                     break;
                 case 'screen':
                     $('[data-fieldname="project"]').val(treeNode.getParentNode().getParentNode().idname);
                     $('[data-fieldname="module"]').val(treeNode.getParentNode().idname);
                     $('[data-fieldname="screen"]').val(treeNode.idname);
+                    listview.refresh();
                     cur_list.refresh();
                     break;
                 case 'bug':
