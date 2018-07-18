6772b581c3c72e5b44bcad1d1026d37e942786d8
Noor Mohamed <noor@valiantsystems.com>
2018-07-18 19:46:10 +0530

    18-6-18

--- pms/pms/controller.py
+++ pms/pms/controller.py
@@ -100,11 +100,13 @@ def senddailytimesheet(allow_guest=True):
 								limit_page_length=200
 								)
 			for q in y.tasks:
-				q.tasks_details = frappe.db.get_list("Task", 
-							fields=['subject', 'status', 'priority', 'description', 'name'],
-							filters={'name': y.name},
-							limit_page_length=200
-							)
+				q.subject = frappe.db.get_value('Task', y.name, 'subject')
+				q.status = frappe.db.get_value('Task', y.name, 'status')
+				q.priority = frappe.db.get_value('Task', y.name, 'priority')
+				q.description = frappe.db.get_value('Task', y.name, 'priority')
+				q.name = frappe.db.get_value('Task', y.name, 'name')
+
+
 
 	# table = '<table cellpading="5" style="border-collapse: collapse;" border="1"><tr><th>Project Name</th><th>Open Tasks</th><th>Closed Tasks</th><th>Open Bugs</th><th>Closed Bugs</th></tr>'
 	# for y in projects:
